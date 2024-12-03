6da9529069ad86cf164cf963e651cf09f8207b89
Michal <010thebest010@gmail.com>
2024-12-03 18:44:04 +0100

    lab10

--- lab10/lab10-nextapp/next.config.mjs
+++ lab10/lab10-nextapp/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default {
+    output: 'export',
+    basePath: '/MichalDanko20436JezykiWys', // Nazwa Twojego repozytorium
+    assetPrefix: '/MichalDanko20436JezykiWys', // Dodaj tę samą nazwę co w basePath
+    images: {
+        unoptimized: true, // Wymagane dla statycznego eksportu obrazów
+    },
+};
+
